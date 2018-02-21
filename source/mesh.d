6d45cd97b88f3216f1af8baf1ec27d909c0d7169
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2018-02-20 22:11:47 -0500

    add some Float3 math

--- source/mesh.d
+++ source/mesh.d
@@ -1,3 +1,4 @@
+import std.math;
 
 struct Float2 {
     float[2] data;
@@ -14,6 +15,13 @@ struct Float2 {
 struct Float3 {
     float[3] data;
 
+    this(float x, float y, float z)
+    {
+        data[0] = x;
+        data[1] = y;
+        data[2] = z;
+    }
+
     @property float x() const { return data[0]; }
     @property ref float x() { return data[0]; }
     @property float x(float value) { data[0] = value; return value; }
@@ -25,6 +33,73 @@ struct Float3 {
     @property float z() const { return data[2]; }
     @property ref float z() { return data[2]; }
     @property float z(float value) { data[2] = value; return value; }
+
+    Float3 opBinary(string op)(const Float3 rhs) const
+    {
+        static assert (op == "+" || op == "-" || op == "*" || op == "/" || op == "%", "Operator "~op~" not implemented");
+        Float3 result;
+        result.data = data[];
+        mixin("result.data[]" ~ op ~ "=rhs.data[];");
+        return result;
+    }
+
+    Float3 opBinary(string op)(float rhs) const
+    {
+        static assert (op == "+" || op == "-" || op == "*" || op == "/" || op == "%", "Operator "~op~" not implemented");
+        Float3 result;
+        result.data = data[];
+        mixin("result.data[]" ~ op ~ "=rhs.data;");
+        return result;
+    }
+
+    void opOpAssign(string op)(const Float3 rhs)
+    {
+        static assert (op == "+" || op == "-" || op == "*" || op == "/" || op == "%", "Operator "~op~" not implemented");
+        mixin("data[]" ~ op ~ "=rhs.data[];");
+    }
+
+    void opOpAssign(string op)(float rhs)
+    {
+        static assert (op == "+" || op == "-" || op == "*" || op == "/" || op == "%", "Operator "~op~" not implemented");
+        mixin("data[]" ~ op ~ "=rhs;");
+    }
+
+    float dot(Float3 rhs) const
+    {
+        return x * rhs.x + y * rhs.y + z * rhs.z;
+    }
+
+    float length() const
+    {
+        return sqrt(dot(this));
+    }
+
+    Float3 cross(const Float3 rhs) const
+    {
+        Float3 result;
+        result.x = y * rhs.z - z * rhs.y;
+        result.y = z * rhs.x - x * rhs.z;
+        result.z = x * rhs.y - y * rhs.x;
+        return result;
+    }
+
+}
+
+unittest {
+    const Float3 vec1 = Float3(1., 0., 2.);
+    assert(vec1.x() == 1. && vec1.y() == 0. && vec1.z() == 2.);
+    const Float3 vec2 = Float3(-1., 0., -2.);
+    assert(vec2.x() == -1. && vec2.y() == 0. && vec2.z() == -2.);
+    const Float3 vec3 = vec1 + vec2;
+    assert(vec3.x() == 0. && vec3.y() == 0. && vec3.z() == 0.);
+
+    immutable float smallEpsilon = 0.0001;
+    Float3 unit1 = Float3(1., 0., 0.);
+    assert(abs(unit1.length() - 1.) < smallEpsilon);
+    Float3 unit2 = Float3(1., 1., 0.);
+    assert(abs(unit2.length() - 1.) > smallEpsilon);
+    unit2 /= unit2.length();
+    assert(abs(unit2.length() - 1.) < smallEpsilon);
 }
 
 struct Face {
