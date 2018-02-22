6ae62f96da33a8efec23728a408ad2fcc3b0bfb7
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2018-02-21 20:34:20 -0500

    added some rendering

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import raster;
 import numeric_alias;
 import derelict.sdl2.sdl;
 import std.stdio;
+import std.conv;
 
 void main()
 {
@@ -31,23 +32,28 @@ void main()
     auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
     scope(exit) SDL_DestroyRenderer(renderer);
 
-    auto texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 160, 144);
+    int width = 160;
+    int height = 144;
+
+    auto texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
     scope(exit) SDL_DestroyTexture(texture);
 
     Mesh mesh = LoadFromFile("model/saria.obj");
 
     Raster raster;
     raster.SetModel(mesh);
-    raster.SetSize(160, 144);
+    raster.SetSize(width, height);
 
-    u32[] surface;
-    surface.length = 160*144;
+    Surface surface;
+    surface.SetSize(width, height);
+    float[] zbuffer;
+    zbuffer.length = width*height;
 
     bool run = true;
     while(run) {
-        raster.Render(surface);
+        raster.Render(surface, zbuffer);
 
-        SDL_UpdateTexture(texture, null, surface.ptr, 160 * u32.sizeof);
+        SDL_UpdateTexture(texture, null, surface.m_data.ptr, to!int(width * u32.sizeof));
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, null, null);
         SDL_RenderPresent(renderer);
--- source/raster.d
+++ source/raster.d
@@ -2,12 +2,106 @@ import loadobj;
 import mesh;
 import numeric_alias;
 import std.stdio;
+import std.math;
+import std.algorithm.mutation;
 
-struct Raster {
-    void Render(u32[] surface)
+struct Surface
+{
+    @property ulong width() const { return m_width; }
+    @property ulong height() const { return m_data.length / m_width; }
+
+    void SetSize(uint w, uint h)
+    {
+        m_data.length = w*h;
+        m_width = w;
+    }
+
+    void SetPixel(uint x, uint y, u32 value)
     in
     {
-        assert(surface.length == m_width * m_height);
+        assert(x <= width);
+        assert(y <= height);
+    }
+    do
+    {
+        m_data[y * m_width + x] = value;
+    }
+
+    u32[] m_data;
+    uint m_width = 0;
+
+    invariant
+    {
+        assert(m_width <= m_data.length);
+    }
+}
+
+void line(ref Surface surface, int x0, int y0, int x1, int y1) { 
+    bool steep = false; 
+    if (abs(x0-x1)<abs(y0-y1)) { 
+        swap(x0, y0); 
+        swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { 
+        swap(x0, x1); 
+        swap(y0, y1); 
+    } 
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    int derror2 = abs(dy)*2; 
+    int error2 = 0; 
+    int y = y0; 
+    for (int x=x0; x<=x1; x++) { 
+        if (steep) { 
+            surface.SetPixel(y, x, 0); 
+        } else { 
+            surface.SetPixel(x, y, 0); 
+        } 
+        error2 += derror2; 
+        if (error2 > dx) { 
+            y += (y1>y0?1:-1); 
+            error2 -= dx*2; 
+        } 
+    } 
+} 
+
+Float3 barycentricCoord(const ref Float3 p, const ref Float3 v0, const ref Float3 v1, const ref Float3 v2)
+{
+    Float3 result = Float3(-1, 0, 0);
+    Float3 v0v1 = v1 - v0; 
+    Float3 v0v2 = v2 - v0; // no need to normalize 
+    Float3 N = v0v1.cross(v0v2); // N 
+    const float area2 = N.length();
+
+    Float3 C; // vector perpendicular to triangle's plane // edge 0 
+    Float3 edge0 = v1 - v0; 
+    Float3 vp0 = p - v0; 
+    C = edge0.cross(vp0); 
+    if (N.dot(C) <= 0.0) 
+        return result; // P is on the right side // edge 1 
+    Float3 edge1 = v2 - v1; 
+    Float3 vp1 = p - v1; 
+    C = edge1.cross(vp1); 
+    result.y = C.length() / area2; 
+    if (N.dot(C) <= 0.0) 
+        return result; // P is on the right side // edge 2 
+    Float3 edge2 = v0 - v2; 
+    Float3 vp2 = p - v2; 
+    C = edge2.cross(vp2); 
+    result.z = C.length() / area2; 
+    if (N.dot(C) <= 0.0) 
+        return result; // P is on the right side; 
+    result.x = 1;
+    return result; // this ray hits the triangle
+}
+
+struct Raster {
+    void Render(ref Surface surface, float[] zbuffer)
+    in
+    {
+        assert(surface.m_data.length == m_width * m_height);
+        assert(surface.m_data.length == zbuffer.length);
     }
     do
     {
@@ -18,10 +112,25 @@ struct Raster {
             foreach(x; 0..m_width)
             {
                 int pixCoord = x + yOffset;
-                if(0 == y % 2)
-                    surface[pixCoord] = 0xFFFFFFFF;
-                else
-                    surface[pixCoord] = 0xFFFF0000;
+                surface.m_data[pixCoord] = 0xFFFFFFFF;
+            }
+        }
+
+        line(surface, 10, 10, 25, 25);
+        line(surface, 25, 25, 25, 50);
+        line(surface, 25, 50, 50, 50);
+
+        Float3 v1 = Float3(56, 56, 0);
+        Float3 v2 = Float3(65, 70, 0);
+        Float3 v0 = Float3(60, 90, 0);
+        foreach(y; 0..m_height)
+        {
+            foreach(x; 0..m_width)
+            {
+                Float3 pixel = Float3(x, y, 0);
+                Float3 bary = barycentricCoord(pixel, v0, v1, v2);
+                if(0 < bary.x)
+                    surface.SetPixel(x,y,0);
             }
         }
     }
